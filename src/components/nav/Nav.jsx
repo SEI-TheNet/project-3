b75085823d1151481d60860b827562f0dbb8ea85
zanaalma <zanaalma1@gmail.com>
2020-01-13 12:47:38 +0300

    fixing the navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Nav.css'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import {Nav} from 'react-bootstrap'
 import {Navbar} from 'react-bootstrap'
 
@@ -12,8 +12,9 @@ const Nave = () => {
   <Navbar.Brand to="/">Home</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
-  <Nav.Link to="/Parks">Parks</Nav.Link>
-  <Nav.Link eventKey="/Exper">Experiences</Nav.Link>
+  <Nav.Link><NavLink to="/Parks">Parks</NavLink></Nav.Link>
+  <Nav.Link><NavLink to="/Exper">Experiences</NavLink></Nav.Link>
+
 
   </Navbar.Collapse>
 </Navbar>
